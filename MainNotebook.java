9f301e97491336b13a1eb3810db1809f00ab7e35
Psey19 <153425698+Psey19@users.noreply.github.com>
2024-05-05 01:38:34 +0500

    Update MainNotebook.java

--- MainNotebook.java
+++ MainNotebook.java
@@ -14,32 +14,33 @@ import java.util.*;
 public class MainNotebook {
     public static void main(String[] args) {
         //Создадим экземпляры класса Notebook
-        Notebook notebook1 = new Notebook("notebook1", 8, 256, "Windows", "Black");
-        Notebook notebook2 = new Notebook("notebook2", 16, 256, "Windows", "Black");
-        Notebook notebook3 = new Notebook("notebook3", 32, 256, "Windows", "White");
-        Notebook notebook4 = new Notebook("notebook4", 8, 512, "Windows", "Black");
-        Notebook notebook5 = new Notebook("notebook5", 16, 512, "Windows", "White");
-        Notebook notebook6 = new Notebook("notebook6", 32, 512, "Windows", "Black");
-        Notebook notebook7 = new Notebook("notebook7", 8, 1024, "Windows", "White");
-        Notebook notebook8 = new Notebook("notebook8", 16, 1024, "Windows", "Black");
-        Notebook notebook9 = new Notebook("notebook9", 32, 1024, "Windows", "Black");
-        Notebook notebook10 = new Notebook("notebook10", 32, 1024, "Windows", "White");
-        Notebook notebook11 = new Notebook("notebook11", 8, 256, "macOS", "Black");
-        Notebook notebook12 = new Notebook("notebook12", 16, 256, "macOS", "White");
-        Notebook notebook13 = new Notebook("notebook13", 32, 256, "macOS", "White");
-        Notebook notebook14 = new Notebook("notebook14", 8, 512, "macOS", "White");
-        Notebook notebook15 = new Notebook("notebook15", 16, 512, "macOS", "Black");
-        Notebook notebook16 = new Notebook("notebook16", 32, 512, "macOS", "White");
-        Notebook notebook17 = new Notebook("notebook17", 8, 1024, "macOS", "White");
-        Notebook notebook18 = new Notebook("notebook18", 16, 1024, "macOS", "White");
-        Notebook notebook19 = new Notebook("notebook19", 32, 1024, "macOS", "Black");
-        Notebook notebook20 = new Notebook("notebook20", 32, 1024, "macOS", "White");
-        Notebook notebook21 = new Notebook("notebook20", 32, 1024, "macOS", "White");
+        Notebook notebook1 = new Notebook("notebook1", 8, 256, "Windows", "Black", "sn101");
+        Notebook notebook2 = new Notebook("notebook2", 16, 256, "Windows", "Black", "sn102");
+        Notebook notebook3 = new Notebook("notebook3", 32, 256, "Windows", "White", "sn103");
+        Notebook notebook4 = new Notebook("notebook4", 8, 512, "Windows", "Black", "sn104");
+        Notebook notebook5 = new Notebook("notebook5", 16, 512, "Windows", "White", "sn105");
+        Notebook notebook6 = new Notebook("notebook6", 32, 512, "Windows", "Black", "sn106");
+        Notebook notebook7 = new Notebook("notebook7", 8, 1024, "Windows", "White", "sn107");
+        Notebook notebook8 = new Notebook("notebook8", 16, 1024, "Windows", "Black", "sn108");
+        Notebook notebook9 = new Notebook("notebook9", 32, 1024, "Windows", "Black", "sn109");
+        Notebook notebook10 = new Notebook("notebook10", 32, 1024, "Windows", "White", "sn110");
+        Notebook notebook11 = new Notebook("notebook11", 8, 256, "macOS", "Black", "sn111");
+        Notebook notebook12 = new Notebook("notebook12", 16, 256, "macOS", "White", "sn112");
+        Notebook notebook13 = new Notebook("notebook13", 32, 256, "macOS", "White", "sn113");
+        Notebook notebook14 = new Notebook("notebook14", 8, 512, "macOS", "White", "sn114");
+        Notebook notebook15 = new Notebook("notebook15", 16, 512, "macOS", "Black", "sn115");
+        Notebook notebook16 = new Notebook("notebook16", 32, 512, "macOS", "White", "sn116");
+        Notebook notebook17 = new Notebook("notebook17", 8, 1024, "macOS", "White", "sn117");
+        Notebook notebook18 = new Notebook("notebook18", 16, 1024, "macOS", "White", "sn118");
+        Notebook notebook19 = new Notebook("notebook19", 32, 1024, "macOS", "Black", "sn119");
+        Notebook notebook20 = new Notebook("notebook20", 32, 1024, "macOS", "White", "sn120");
+        Notebook notebook21 = new Notebook("notebook20", 32, 1024, "macOS", "White", "sn120");
+        Notebook notebook22 = new Notebook("notebook20", 32, 1024, "macOS", "White", "sn122");
 
         //Создадим множество для хранения всего ассортимента ноутбуков из экземпляров класса Notebook
         Set<Notebook> notebooks = new HashSet<>(Arrays.asList(notebook1, notebook2, notebook3, notebook4, notebook5,
                 notebook6, notebook7, notebook8, notebook9, notebook10, notebook11, notebook12, notebook13,
-                notebook14, notebook15, notebook16, notebook17, notebook18, notebook19, notebook20, notebook21));
+                notebook14, notebook15, notebook16, notebook17, notebook18, notebook19, notebook20, notebook21, notebook22));
         filterParamsNotebook(notebooks);
     }
 
